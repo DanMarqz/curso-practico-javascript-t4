26862c4f3d9054014305cbfa3e2cd6d753c6dc46
Danmarqz <danielarturo.marquez@gmail.com>
2022-02-05 15:28:40 -0400

    finished

---
+++ README.md
@@ -0,0 +1,4 @@
+Primer paso: Definir formulas  
+Segundo paso: Implementar formulas en JS  
+Tercer paso: Crear funciones  
+Cuarto paso: Implementar JS en HTML
---
+++ salarios.js
@@ -0,0 +1,54 @@
+const colombia = []
+
+colombia.push({
+  name: 'Sofi',
+  salary: 1500
+})
+colombia.push({
+  name: 'Ramona',
+  salary: 1800
+})
+colombia.push({
+  name: 'Sheryl',
+  salary: 1000
+})
+colombia.push({
+  name: 'Miguel',
+  salary: 2000
+})
+colombia.push({
+  name: 'Armando',
+  salary: 1700
+})
+colombia.push({
+  name: 'Daniel',
+  salary: 2100
+})
+colombia.push({
+  name: 'Betty',
+  salary: 2600
+})
+colombia.push({
+  name: 'Nicolas',
+  salary: 1700
+})
+colombia.push({
+  name: 'Puerco',
+  salary: 1350
+})
+colombia.push({
+  name: 'Amanda',
+  salary: 1900
+})
+colombia.push({
+  name: 'Antonio',
+  salary: 3500
+})
+colombia.push({
+  name: 'Carlos',
+  salary: 3000
+})
+colombia.push({
+  name: 'Jose',
+  salary: 25000
+})
